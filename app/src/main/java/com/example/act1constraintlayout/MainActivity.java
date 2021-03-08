4e61e7b3ab66fafb1e7fdd1b6c3131d70fe154c1
Bima Bayu Saputra <bima.bayu.ft19@mail.umy.ac.id>
2021-03-08 21:54:56 +0700

    Activity 2 even button onclick listener by Bima Bayu Saputra NIM 20190140011

--- app/src/main/java/com/example/act1constraintlayout/MainActivity.java
+++ app/src/main/java/com/example/act1constraintlayout/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.act1constraintlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btSignin;
+    EditText edemail, edpassword;
+    String Name,Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btSignin = findViewById(R.id.btSignin);
+        edemail = findViewById(R.id.edEmail);
+        edpassword = findViewById(R.id.edPassword);
+
+        btSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Name = edemail.getText().toString();
+                Password = edpassword.getText().toString();
+
+                if (Name.equals("bimabayusaputra111@gmail.com") && Password.equals("00000000")){
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Login SUKSES!!!" + "", Toast.LENGTH_LONG);
+                    t.show();
+                }
+
+                else if(!Name.equals("bimabayusaputra111@gmail.com") && Password.equals("00000000")){
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Email SALAH!!!", Toast.LENGTH_LONG);
+                    t.show();
+                }
+                else if(Name.equals("bimabayusaputra111@gmail.com") && !Password.equals("00000000")){
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Password SALAH!!!",Toast.LENGTH_LONG);
+                    t.show();
+                }
+                else{
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Email atau Password SALAH!!!", Toast.LENGTH_LONG);
+                    t.show();
+                }
+            }
+        });
     }
 }
